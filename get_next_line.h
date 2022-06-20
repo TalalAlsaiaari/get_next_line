afbc41eba909876eb8a61aadb483fb52cda6b1b8
Talal Alsaiaari <talsaiaa@lab1r1s5.42abudhabi.ae>
2022-06-20 22:55:36 +0400

    GNL submission

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: talsaiaa <talsaiaa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/20 02:11:44 by talsaiaa          #+#    #+#             */
+/*   Updated: 2022/06/20 22:25:23 by talsaiaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef     GET_NEXT_LINE_H
+# define     GET_NEXT_LINE_H
+# include    <unistd.h>
+# include    <stdlib.h>
+# include    <fcntl.h>
+# include    <stdio.h>
+
+int		ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *s1);
+char	*get_next_line(int fd);
+char	*get_the_line(char *str);
+char	*get_the_remainder(char *str);
+char	*reading(int fd, char *str);
+
+#endif
